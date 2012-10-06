e7b6c0f7759bc62c6b0aee38b3c9e30a10b22a1c
Jyoti Ranjan Pattnaik <jyotiranjanpattnaik@gmail.com>
2012-10-07 02:03:36 +0530

    added one line

--- src/com/mysite/HelloWorld.java
+++ src/com/mysite/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 		System.out.println("hello world.");
 		System.out.println("project name should start from Capital letter and without java. It should be First");
 		System.out.println("Now I am making this change. I have to reflect this change in git hub repo.");
-
+		System.out.println("This change is from other system");
 	}
 
 }
