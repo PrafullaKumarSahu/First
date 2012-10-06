85bcd8f7b69c8ddc8305038b315983c9d00ca574
pk <info@pkrules.in>
2012-10-06 16:20:08 -0400

    my second commit

--- src/com/mysite/HelloWorld.java
+++ src/com/mysite/HelloWorld.java
@@ -2,13 +2,10 @@ package com.mysite;
 
 public class HelloWorld {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		System.out.println("hello world.");
 		System.out.println("project name should start from Capital letter and without java. It should be First");
-		
+		System.out.println("Now I am making this change. I have to reflect this change in git hub repo.");
 
 	}
 
