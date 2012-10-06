2e59ada50532715532aa72fd7abe2df17a85387b
pk <info@pkrules.in>
2012-10-06 16:05:44 -0400

    this is my  first commit

---
+++ src/com/mysite/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.mysite;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello world.");
+		System.out.println("project name should start from Capital letter and without java. It should be First");
+		
+
+	}
+
+}
