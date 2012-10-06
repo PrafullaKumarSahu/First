40f04253e12f8161817505782a5fff5965820f46
pk <info@pkrules.in>
2012-10-06 16:42:12 -0400

    my third commit

--- src/com/mysite/HelloWorld.java
+++ src/com/mysite/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		System.out.println("hello world.");
 		System.out.println("project name should start from Capital letter and without java. It should be First");
 		System.out.println("Now I am making this change. I have to reflect this change in git hub repo.");
+		System.out.println("made a change by adding a line to it.");
 
 	}
 
